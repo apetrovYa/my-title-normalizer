45f6e686669df397b1a399875e99dc97ed7ebfc8
Andrei Petrov <apetrov.ya@yandex.ru>
2020-06-09 23:24:36 +0200

    Add a simple Bash wrapper. after npm install -g the execution of the tool resolves in zsh permission denied on Mac

--- index.js
+++ index.js
@@ -7,10 +7,12 @@ cli
     .command('[input]', 'Transform the given title')
     .example('my-title-normalizer "This Text Will Be Transformed"')
     .action((input) => {
-        console.log("ORIGINAL TITLE: " + input);
-        let t = new Title(input);
-        t.normalize();
-        console.log("NORMALIZED TITLE: " + t.getText());
+        if (typeof input !== 'undefined') {
+            console.log("ORIGINAL TITLE: " + input);
+            let t = new Title(input);
+            t.normalize();
+            console.log("NORMALIZED TITLE: " + t.getText());
+        }
     });
 cli.help();
 cli.version('1.0.0');
